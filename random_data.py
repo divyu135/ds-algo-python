2c2732ebccbef8d790ea8ccb8e8ace16f575f93c
Divyajeet Chauhan <71348527+divyu135@users.noreply.github.com>
2023-02-28 16:28:15 -0500

    Update random_data.py

--- random_data.py
+++ random_data.py
@@ -1,47 +1,55 @@
-def generate_random_data(schema):
-    """
-    Generate random data for the given schema.
-    """
-    if isinstance(schema, StructType):
-        return generate_struct(schema)
-
-    elif isinstance(schema, ArrayType):
-        return [generate_random_data(schema.elementType) for _ in range(random.randint(0, 5))]
-
-    else:
-        return generate_primitive(schema)
-
+from pyspark.sql.types import *
 
 def generate_struct(schema):
     """
-    Generate random StructType data for the given schema.
+    Generates random data for a StructType schema.
     """
-    fields = []
+    struct_data = {}
     for field in schema.fields:
-        name = field.name
-        data_type = field.dataType
-        value = generate_random_data(data_type)
-        fields.append((name, value))
-    return dict(fields)
+        field_name = field.name
+        field_type = field.dataType
+        if isinstance(field_type, StructType):
+            struct_data[field_name] = generate_struct(field_type)
+        else:
+            struct_data[field_name] = generate_random_data(field_type)
+    return struct_data
 
-
-def generate_primitive(schema):
+def generate_random_data(data_type):
     """
-    Generate random primitive data for the given schema.
+    Generates random data for a given data type.
     """
-    if isinstance(schema, StringType):
-        return "string" + str(random.randint(1, 10))
-    elif isinstance(schema, IntegerType):
-        return random.randint(1, 100)
-    elif isinstance(schema, LongType):
-        return random.randint(1, 1000)
-    elif isinstance(schema, DoubleType):
-        return random.uniform(1, 10)
-    elif isinstance(schema, FloatType):
-        return random.uniform(1, 10)
-    elif isinstance(schema, BooleanType):
-        return bool(random.getrandbits(1))
-    elif isinstance(schema, DateType):
-        return str(random.randint(2000, 2023)) + "-" + str(random.randint(1, 12)) + "-" + str(random.randint(1, 28))
+    if isinstance(data_type, StringType):
+        return ''.join(random.choice(string.ascii_letters) for i in range(10))
+    elif isinstance(data_type, IntegerType):
+        return random.randint(0, 100)
+    elif isinstance(data_type, LongType):
+        return random.randint(0, 100000000)
+    elif isinstance(data_type, DoubleType):
+        return random.random() * 100
+    elif isinstance(data_type, BooleanType):
+        return random.choice([True, False])
+    elif isinstance(data_type, DateType):
+        return datetime.date.today() - datetime.timedelta(days=random.randint(0, 365))
+    elif isinstance(data_type, TimestampType):
+        return datetime.datetime.now() - datetime.timedelta(seconds=random.randint(0, 3600))
+    elif isinstance(data_type, ArrayType):
+        return [generate_random_data(data_type.elementType) for i in range(3)]
+    elif isinstance(data_type, MapType):
+        return {generate_random_data(data_type.keyType): generate_random_data(data_type.valueType) for i in range(3)}
     else:
         return None
+
+def generate_data(schema):
+    """
+    Generates random data for a given schema.
+    """
+    table_data = []
+    for field in schema.fields:
+        field_name = field.name
+        field_type = field.dataType
+        if isinstance(field_type, StructType):
+            struct_data = generate_struct(field_type)
+            table_data.append(struct_data)
+        else:
+            table_data.append(generate_random_data(field_type))
+    return table_data
