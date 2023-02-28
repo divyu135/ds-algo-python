305502293220bb457cdb85d770763d7db4d93867
Divyajeet Chauhan <71348527+divyu135@users.noreply.github.com>
2023-02-28 15:04:20 -0500

    Update random_data.py

--- random_data.py
+++ random_data.py
@@ -1,46 +1,45 @@
-import random
-from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType, DoubleType, IntegerType, LongType, DecimalType, FloatType, DateType, TimestampType, StructType, ArrayType, StructField
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import rand, expr
+from pyspark.sql.types import *
 
-# Define a function to generate random data based on column data type
-def generate_random_data(col_type, col_range=None):
-    if col_type == StringType():
-        return ''.join(random.choices(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], k=random.randint(5, 10)))
-    elif col_type == DoubleType() or col_type == FloatType() or col_type == DecimalType():
-        return random.uniform(col_range[0], col_range[1])
-    elif col_type == IntegerType() or col_type == LongType():
-        return random.randint(col_range[0], col_range[1])
-    elif col_type == DateType():
-        return str(random.randint(col_range[0], col_range[1]))
-    elif col_type == TimestampType():
-        return str(random.randint(col_range[0], col_range[1]))
+# Define a function to generate random data for each column type
+def generate_random_data(data_type):
+    if isinstance(data_type, StructType):
+        return expr("struct(" + ",".join([generate_random_data(field.dataType) for field in data_type.fields]) + ")")
+    elif isinstance(data_type, ArrayType):
+        return expr("array(" + generate_random_data(data_type.elementType) + ")")
+    elif isinstance(data_type, MapType):
+        return expr("map_from_arrays(array(" + generate_random_data(data_type.keyType) + "), array(" + generate_random_data(data_type.valueType) + "))")
+    elif isinstance(data_type, StringType):
+        return expr("concat('string_', cast(rand() * 1000000000 as bigint))")
+    elif isinstance(data_type, IntegerType):
+        return expr("cast(rand() * 1000000000 as int)")
+    elif isinstance(data_type, LongType):
+        return expr("cast(rand() * 1000000000000000000 as bigint)")
+    elif isinstance(data_type, FloatType):
+        return expr("rand()")
+    elif isinstance(data_type, DoubleType):
+        return expr("rand() * 1000000")
+    elif isinstance(data_type, DecimalType):
+        return expr("cast(rand() * 1000000 as decimal(10, 2))")
+    elif isinstance(data_type, TimestampType):
+        return expr("date_add(to_utc_timestamp('1970-01-01', 'UTC'), cast(rand() * 1000000000 as bigint))")
+    elif isinstance(data_type, DateType):
+        return expr("date_add(to_date('1970-01-01'), cast(rand() * 10000 as int))")
     else:
-        return None
+        return expr("null")
 
-# Define a function to recursively generate random data for struct and array types
-def generate_random_data_for_complex_types(col_type, col_range=None):
-    if isinstance(col_type, StructType):
-        return [generate_random_data_for_complex_types(f.dataType, None) for f in col_type.fields]
-    elif isinstance(col_type, ArrayType):
-        return [generate_random_data_for_complex_types(col_type.elementType, None)]
-    else:
-        return generate_random_data(col_type, col_range)
+# Create a SparkSession
+spark = SparkSession.builder \
+    .appName("RandomSampleDataGenerator") \
+    .enableHiveSupport() \
+    .getOrCreate()
 
-# Define a UDF to generate random data for each column based on column data type and range
-generate_random_data_udf = udf(generate_random_data_for_complex_types, StructType([
-    StructField("col", StructType([
-        StructField("type", StringType(), True),
-        StructField("data", col.dataType, True)
-    ]), True)
-]))
+# Read 100 rows from the Hive table
+df = spark.sql("SELECT * FROM my_hive_table LIMIT 100")
 
-# Create a list of StructFields representing the columns of the DataFrame with their data type
-cols = [StructField(c.name, StructType([
-    StructField("type", StringType(), True),
-    StructField("data", c.dataType, True)
-]), True) for c in df.schema]
+# Generate random sample data based on the schema of the DataFrame
+sample_df = df.select([generate_random_data(field.dataType).alias(field.name) for field in df.schema.fields])
 
-# Create a new DataFrame with the same schema as the original, but with the data replaced by random data
-dummy_df = df.select([generate_random_data_udf(
-    StructType([cols[i]]).alias(cols[i].name)
-).alias(cols[i].name) for i in range(len(cols))])
+# Show the sample data
+sample_df.show()
