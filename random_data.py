8893fa8fe2ad2ee2239a28f78069cbea5ea9cbe1
Divyajeet Chauhan <71348527+divyu135@users.noreply.github.com>
2023-02-28 15:54:52 -0500

    Update random_data.py

--- random_data.py
+++ random_data.py
@@ -1,48 +1,19 @@
-from pyspark.sql.functions import udf, expr
-from pyspark.sql.types import *
-
-def generate_random_data(data_type):
-    if isinstance(data_type, StringType):
-        return udf(lambda: "test string")()
-    elif isinstance(data_type, IntegerType):
-        return udf(lambda: 1)()
-    elif isinstance(data_type, LongType):
-        return udf(lambda: 1)()
-    elif isinstance(data_type, FloatType):
-        return udf(lambda: 1.0)()
-    elif isinstance(data_type, DoubleType):
-        return udf(lambda: 1.0)()
-    elif isinstance(data_type, BooleanType):
-        return udf(lambda: True)()
-    elif isinstance(data_type, DateType):
-        return udf(lambda: "2022-01-01")()
-    elif isinstance(data_type, TimestampType):
-        return udf(lambda: "2022-01-01 00:00:00")()
-    elif isinstance(data_type, DecimalType):
-        return udf(lambda: Decimal(1))()
-    elif isinstance(data_type, ArrayType):
-        return udf(lambda: [generate_random_data(data_type.elementType) for i in range(3)])()
-    elif isinstance(data_type, MapType):
-        return udf(lambda: {"key": generate_random_data(data_type.keyType), "value": generate_random_data(data_type.valueType)})()
-    elif isinstance(data_type, StructType):
-        fields_expr = [expr("`" + f.name + "`") for f in data_type.fields]
-        return expr("struct(" + ",".join([generate_random_data(field.dataType) for field in data_type.fields]) + ")").select(*fields_expr)
-    elif isinstance(data_type, NullType):
-        return udf(lambda: None)()
-    else:
-        raise ValueError("Unsupported data type: " + str(data_type))
-
-
+from pyspark.sql.functions import rand, randn
 from pyspark.sql import SparkSession
 
-# Create a SparkSession
-spark = SparkSession.builder.appName("Generate Random Data").enableHiveSupport().getOrCreate()
+# create spark session
+spark = SparkSession.builder.appName("RandomDataGenerator").getOrCreate()
 
-# Read the schema of the Hive table
-table_schema = spark.table("<database_name>.<table_name>").schema
+# get column names from schema
+column_names = [field.name for field in table_schema.fields]
 
-# Generate random data based on the schema
-random_data = generate_random_data(table_schema)
+# generate random data
+df = spark.range(1000)
+for column in column_names:
+    df = df.withColumn(column, rand())
 
-# Show the generated data
-random_data.show()
+# create dataframe with correct schema
+df = spark.createDataFrame(df.rdd, schema=table_schema)
+
+# show dataframe
+df.show()
