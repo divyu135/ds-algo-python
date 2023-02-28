250000b9d723046c6cd261fa28250d7e31755837
Divyajeet Chauhan <71348527+divyu135@users.noreply.github.com>
2023-02-28 15:30:42 -0500

    Update random_data.py

--- random_data.py
+++ random_data.py
@@ -6,42 +6,48 @@ from pyspark.sql.types import *
 def generate_random_data(data_type):
     if isinstance(data_type, StructType):
         fields_expr = [generate_random_data(field.dataType).alias(field.name) for field in data_type.fields]
-        return expr("struct(" + ",".join([f"`{f.name}`" for f in data_type.fields]) + ")").selectExpr(*[f"`{f.name}`" for f in data_type.fields])
+        return expr("struct(" + ",".join([f"`{f.name}`" for f in data_type.fields]) + ")").select(*fields_expr)
     elif isinstance(data_type, ArrayType):
-        return expr("array(" + generate_random_data(data_type.elementType) + ")")
+        return expr("array(" + generate_random_data(data_type.elementType).cast("string") + ")")
     elif isinstance(data_type, MapType):
-        return expr("map_from_arrays(array(" + generate_random_data(data_type.keyType) + "), array(" + generate_random_data(data_type.valueType) + "))")
+        key_expr = generate_random_data(data_type.keyType).cast("string")
+        value_expr = generate_random_data(data_type.valueType).cast("string")
+        return expr("map_from_arrays(array(" + key_expr + "), array(" + value_expr + "))")
     elif isinstance(data_type, StringType):
-        return expr("concat('string_', cast(rand() * 1000000000 as bigint))")
+        return expr("concat('string_', cast(rand() * 100 as int))")
     elif isinstance(data_type, IntegerType):
-        return expr("cast(rand() * 1000000000 as int)")
-    elif isinstance(data_type, LongType):
-        return expr("cast(rand() * 1000000000000000000 as bigint)")
-    elif isinstance(data_type, FloatType):
-        return expr("rand()")
+        return expr("cast(rand() * 100 as int)")
     elif isinstance(data_type, DoubleType):
-        return expr("rand() * 1000000")
-    elif isinstance(data_type, DecimalType):
-        return expr("cast(rand() * 1000000 as decimal(10, 2))")
+        return expr("rand()")
+    elif isinstance(data_type, BooleanType):
+        return expr("(rand() > 0.5)")
     elif isinstance(data_type, TimestampType):
-        return expr("date_add(to_utc_timestamp('1970-01-01', 'UTC'), cast(rand() * 1000000000 as bigint))")
+        return expr("from_unixtime(rand() * 1000000000)")
     elif isinstance(data_type, DateType):
-        return expr("date_add(to_date('1970-01-01'), cast(rand() * 10000 as int))")
+        return expr("from_unixtime(rand() * 1000000000, 'yyyy-MM-dd')").cast("date")
     else:
-        return expr("null")
-
+        raise ValueError(f"Unsupported data type: {data_type}")
 
 # Create a SparkSession
 spark = SparkSession.builder \
-    .appName("RandomSampleDataGenerator") \
+    .appName("Generate random data for a Hive table") \
     .enableHiveSupport() \
     .getOrCreate()
 
-# Read 100 rows from the Hive table
-df = spark.sql("SELECT * FROM my_hive_table LIMIT 100")
+# Define the name of the Hive table
+table_name = "my_table"
 
-# Generate random sample data based on the schema of the DataFrame
-sample_df = df.select([generate_random_data(field.dataType).alias(field.name) for field in df.schema.fields])
+# Load the schema of the Hive table
+table_schema = spark.table(table_name).schema
 
-# Show the sample data
-sample_df.show()
+# Get 100 rows from the Hive table and store them in a DataFrame
+df = spark.sql(f"SELECT * FROM {table_name} LIMIT 100")
+
+# Generate random data for each column based on its data type
+random_exprs = [generate_random_data(field.dataType).alias(field.name) for field in table_schema.fields]
+
+# Add the random data to the DataFrame
+df_with_random_data = df.select(*random_exprs)
+
+# Show the first 10 rows of the DataFrame
+df_with_random_data.show(10)
