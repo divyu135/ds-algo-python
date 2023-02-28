aac548c3486d8f4b1584e5e922bb25fa1b2d8abb
Divyajeet Chauhan <71348527+divyu135@users.noreply.github.com>
2023-02-28 12:03:49 -0500

    Update random_data.py

--- random_data.py
+++ random_data.py
@@ -2,134 +2,91 @@ import random
 import string
 import datetime
 import decimal
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, ArrayType
 
-from pyspark.sql.functions import col
-from pyspark.sql.types import StructType, ArrayType
 
-# Define function to check if a string represents an integer
-def is_integer(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-# Define function to get the distinct characters in a string column
-def get_character_range(df, column):
-    distinct_chars = set()
-    for row in df.select(column).distinct().collect():
-        if row[column] is not None:
-            distinct_chars.update(list(row[column]))
-    if len(distinct_chars) > 0:
-        min_char = ord(min(distinct_chars))
-        max_char = ord(max(distinct_chars))
-        return (min_char, max_char)
-    else:
-        return None
-
-# Define function to handle nested struct columns
-def generate_sample_data_for_struct(df, column):
-    # Get schema of the struct column
-    struct_schema = df.schema[column].dataType
-    # Generate sample data for each field in the struct
-    sample_data = {}
-    for field in struct_schema.fields:
-        field_name = "{}.{}".format(column, field.name)
-        if isinstance(field.dataType, StructType):
-            # If field is nested struct, recursively generate sample data
-            sample_data[field_name] = generate_sample_data_for_struct(df, field_name)
-        elif isinstance(field.dataType, ArrayType):
-            # If field is array, generate a random array of random length
-            array_len = random.randint(1, 5)
-            array_data = []
-            for _ in range(array_len):
-                array_data.append(generate_sample_data_for_type(df, field_name))
-            sample_data[field_name] = array_data
-        else:
-            # Otherwise, generate sample data for the field
-            sample_data[field_name] = generate_sample_data_for_type(df, field_name)
-    return sample_data
-
-# Define function to generate sample data for a given column type
-def generate_sample_data_for_type(df, column):
-    # Get range of column values
+def get_table_column_ranges(spark, table_name):
+    df = spark.table(table_name)
     column_ranges = {}
-    character_ranges = {}
-    for col_type in df.schema[column].dataType:
-        if isinstance(col_type, StructType):
-            # If column is struct, recursively generate sample data for each field in the struct
-            column_ranges[column] = generate_sample_data_for_struct(df, column)
-        elif isinstance(col_type, ArrayType):
-            # If column is array, generate a random array of random length
-            array_len = random.randint(1, 5)
-            array_data = []
-            for _ in range(array_len):
-                array_data.append(generate_sample_data_for_type(df, column))
-            column_ranges[column] = array_data
-        elif isinstance(col_type, str):
-            # If column is string, get range of string lengths
-            min_len = df.agg({column: "min"}).collect()[0][0]
-            max_len = df.agg({column: "max"}).collect()[0][0]
-            column_ranges[column] = (min_len, max_len)
-            # If column has a range of characters, get range of character values
-            char_range = get_character_range(df, column)
-            if char_range is not None:
-                character_ranges[column] = char_range
-        elif isinstance(col_type, (datetime.date, datetime.datetime)):
-            # If column is date or datetime, get range of dates/times
-            column_ranges[column] = (df.agg({"{}".format(column): "min"}).collect()[0][0], df.agg({"{}".format(column): "max"}).collect()[0][0])
-        elif isinstance(col_type, decimal.Decimal):
-            # If column is decimal, get range of decimal values
-            column_ranges[column] = (float(df.agg({"{}".format(column): "min"}).collect()[0][0]), float(df.agg({"{}".format(column): "max"}).collect()[0][0]))
-        elif isinstance(col_type, bool):
-            # If column is boolean, generate a random boolean value
-            column_ranges[column] = (False, True)
-        elif isinstance(col_type, (int, float)):
-            # If column is numeric, get range of numeric values
-            column_ranges[column] = (df.agg({"{}".format(column): "min"}).collect()[0][0], df.agg({"{}".format(column): "max"}).collect()[0][0])
-
-    # Generate sample data based on column type and range
-    sample_data = None
-    for col_type in df.schema[column].dataType:
-        if isinstance(col_type, StructType):
-            # If column is struct, recursively generate sample data for each field in the struct
-            sample_data = generate_sample_data_for_struct(df, column)
-        elif isinstance(col_type, ArrayType):
-            # If column is array, generate a random array of random length
-            array_len = random.randint(1, 5)
-            array_data = []
-            for _ in range(array_len):
-                array_data.append(generate_sample_data_for_type(df, column))
-            sample_data = array_data
-        elif isinstance(col_type, str):
-            # If column is string, generate a random string of random length and characters
-            if column in character_ranges:
-                # If column has a range of characters, generate random string within that range
-                min_char, max_char = character_ranges[column]
-                length = random.randint(*column_ranges[column])
-                sample_data = "".join([chr(random.randint(min_char, max_char)) for _ in range(length)])
+    for column in df.columns:
+        column_type = df.schema[column].dataType
+        if isinstance(column_type, (int, float, decimal.Decimal)):
+            column_ranges[column] = (df.agg({column: "min"}).collect()[0][0], df.agg({column: "max"}).collect()[0][0])
+        elif isinstance(column_type, str):
+            distinct_vals = df.select(column).distinct().rdd.map(lambda r: r[0]).collect()
+            if len(distinct_vals) == 1:
+                column_ranges[column] = len(distinct_vals[0])
+            elif all(val.isdigit() for val in distinct_vals):
+                column_ranges[column] = (min(int(val) for val in distinct_vals), max(int(val) for val in distinct_vals))
+            elif all(len(val) == 1 for val in distinct_vals):
+                column_ranges[column] = (min(ord(val) for val in distinct_vals), max(ord(val) for val in distinct_vals))
             else:
-                # If column does not have a range of characters, generate random string with alphanumeric characters
-                length = random.randint(*column_ranges[column])
-                sample_data = "".join([random.choice(string.ascii_letters + string.digits) for _ in range(length)])
-        elif isinstance(col_type, (datetime.date, datetime.datetime)):
-            # If column is date or datetime, generate a random date/time within the range
-            min_date, max_date = column_ranges[column]
-            delta = max_date - min_date
-            random_date = min_date + datetime.timedelta(seconds=random.randint(0, delta.total_seconds()))
-            sample_data = random_date
-        elif isinstance(col_type, decimal.Decimal):
-            # If column is decimal, generate a random decimal value within the range
-            min_val, max_val = column_ranges[column]
-            decimal_places = col_type.as_tuple().exponent * -1
-            sample_data = round(random.uniform(min_val, max_val), decimal_places)
-        elif isinstance(col_type, bool):
-            # If column is boolean, generate a random boolean value
-            sample_data = random.choice([False, True])
-        elif isinstance(col_type, (int, float)):
-            # If column is numeric, generate a random value within the range
-            sample_data = random.uniform(*column_ranges[column])
-            if col_type == int:
-                sample_data = int(sample_data)
-    
-    return sample_data
+                column_ranges[column] = None
+        elif isinstance(column_type, (datetime.date, datetime.datetime)):
+            column_ranges[column] = (df.agg({column: "min"}).collect()[0][0], df.agg({column: "max"}).collect()[0][0])
+        elif isinstance(column_type, bool):
+            column_ranges[column] = None
+        elif isinstance(column_type, StructType):
+            column_ranges[column] = get_struct_column_ranges(column_type)
+        elif isinstance(column_type, ArrayType) and isinstance(column_type.elementType, StructType):
+            column_ranges[column] = [get_struct_column_ranges(column_type.elementType)]
+        else:
+            column_ranges[column] = None
+    return column_ranges
+
+
+def get_struct_column_ranges(struct_type):
+    struct_ranges = {}
+    for field in struct_type.fields:
+        field_type = field.dataType
+        if isinstance(field_type, StructType):
+            struct_ranges[field.name] = get_struct_column_ranges(field_type)
+        elif isinstance(field_type, ArrayType) and isinstance(field_type.elementType, StructType):
+            struct_ranges[field.name] = [get_struct_column_ranges(field_type.elementType)]
+        else:
+            struct_ranges[field.name] = (None, None)
+    return struct_ranges
+
+
+def generate_sample_data_for_type(col_type, column_ranges):
+    if isinstance(col_type, (int, float, decimal.Decimal)):
+        return random.uniform(column_ranges[0], column_ranges[1])
+    elif isinstance(col_type, str):
+        if is_character_range(column_ranges):
+            return "".join(random.choices(string.ascii_letters + string.digits, k=column_ranges))
+        elif isinstance(column_ranges, tuple) and all(isinstance(val, int) for val in column_ranges):
+            return str(random.randint(column_ranges[0], column_ranges[1]))
+        else:
+            return None
+    elif isinstance(col_type, (datetime.date, datetime.datetime)):
+        epoch_start = col_type.fromisoformat('1970-01-01')
+        start_seconds = (column_ranges[0] - epoch_start).total_seconds()
+        end_seconds = (column_ranges[1] - epoch_start).total_seconds()
+        return datetime.datetime.fromtimestamp(random.uniform(start_seconds, end_seconds)).isoformat()
+    elif isinstance(col_type, bool):
+        return random.choice([True, False])
+    elif isinstance(col_type, StructType):
+        return {field.name: generate
+
+
+
+from pyspark.sql import SparkSession
+
+# Create SparkSession
+spark = SparkSession.builder.appName("Generate Sample Data").getOrCreate()
+
+# Specify Hive table name
+table_name = "mytable"
+
+# Get column ranges for table
+column_ranges = get_table_column_ranges(spark, table_name)
+
+# Generate sample data for each column
+sample_data = {}
+for col_name, col_range in column_ranges.items():
+    sample_data[col_name] = generate_sample_data_for_type(col_range)
+
+# Print sample data
+for col_name, col_data in sample_data.items():
+    print(f"{col_name}: {col_data}")
