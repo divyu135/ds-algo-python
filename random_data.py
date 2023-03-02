6ecef20cd7cb5ea7cb908f700ba20d7f03c8ca61
Divyajeet Chauhan <71348527+divyu135@users.noreply.github.com>
2023-03-02 11:33:30 -0500

    Update random_data.py

--- random_data.py
+++ random_data.py
@@ -1,66 +1,68 @@
-from pyspark.sql.types import *
-import random
-import string
-import datetime
+def get_min_max(df, field_name):
+    """
+    Returns the minimum and maximum values for a given column in a Spark DataFrame.
+    """
+    col_min, col_max = df.selectExpr("min({})".format(field_name), "max({})".format(field_name)).first()
+    if col_min is None or col_max is None:
+        return None, None
+    return col_min, col_max
 
-def generate_struct(schema, df):
+def get_field_min_max(df, field, parent_name=None):
     """
-    Generates random data for a StructType schema based on the min/max range in a Spark DataFrame.
+    Recursively gets the minimum and maximum values for a field in a StructType schema.
     """
-    struct_data = {}
-    for field in schema.fields:
-        field_name = field.name
-        field_type = field.dataType
-        if isinstance(field_type, StructType):
-            struct_data[field_name] = generate_struct(field_type, df)
-        else:
-            col_min, col_max = df.selectExpr("min({})".format(field_name), "max({})".format(field_name)).first()
-            struct_data[field_name] = generate_random_data(field_type, col_min, col_max)
-    return struct_data
-
-def generate_random_data(data_type, col_min, col_max):
-    """
-    Generates random data for a given data type based on the min/max range.
-    """
-    if isinstance(data_type, StringType):
-        return ''.join(random.choice(string.ascii_letters) for i in range(10))
-    elif isinstance(data_type, IntegerType):
-        return random.randint(col_min, col_max)
-    elif isinstance(data_type, LongType):
-        return random.randint(col_min, col_max)
-    elif isinstance(data_type, DoubleType):
-        return random.uniform(col_min, col_max)
-    elif isinstance(data_type, BooleanType):
-        return random.choice([True, False])
-    elif isinstance(data_type, DateType):
-        col_min_date = datetime.datetime.strptime(col_min, "%Y-%m-%d").date()
-        col_max_date = datetime.datetime.strptime(col_max, "%Y-%m-%d").date()
-        random_date = col_min_date + datetime.timedelta(days=random.randint(0, (col_max_date - col_min_date).days))
-        return random_date
-    elif isinstance(data_type, TimestampType):
-        col_min_time = datetime.datetime.strptime(col_min, "%Y-%m-%d %H:%M:%S")
-        col_max_time = datetime.datetime.strptime(col_max, "%Y-%m-%d %H:%M:%S")
-        random_time = col_min_time + datetime.timedelta(seconds=random.randint(0, (col_max_time - col_min_time).total_seconds()))
-        return random_time
-    elif isinstance(data_type, ArrayType):
-        return [generate_random_data(data_type.elementType, col_min, col_max) for i in range(3)]
-    elif isinstance(data_type, MapType):
-        return {generate_random_data(data_type.keyType, col_min, col_max): generate_random_data(data_type.valueType, col_min, col_max) for i in range(3)}
+    field_name = field.name
+    field_type = field.dataType
+    full_field_name = field_name if parent_name is None else parent_name + "." + field_name
+    if isinstance(field_type, StructType):
+        min_max_values = {}
+        for sub_field in field_type.fields:
+            sub_min_max_values = get_field_min_max(df, sub_field, full_field_name)
+            min_max_values.update(sub_min_max_values)
+        return min_max_values
     else:
-        return None
+        col_min, col_max = get_min_max(df, full_field_name)
+        return {full_field_name: (col_min, col_max)}
+
+def get_schema_min_max(df, schema):
+    """
+    Gets the minimum and maximum values for all fields in a StructType schema.
+    """
+    schema_min_max = {}
+    for field in schema.fields:
+        field_min_max = get_field_min_max(df, field)
+        schema_min_max.update(field_min_max)
+    return schema_min_max
 
 def generate_data(schema, df):
     """
     Generates random data for a given schema based on the min/max range in a Spark DataFrame.
     """
+    schema_min_max = get_schema_min_max(df, schema)
     table_data = []
     for field in schema.fields:
         field_name = field.name
         field_type = field.dataType
         if isinstance(field_type, StructType):
-            struct_data = generate_struct(field_type, df)
+            struct_data = generate_struct(field_type, schema_min_max)
             table_data.append(struct_data)
         else:
-            col_min, col_max = df.selectExpr("min({})".format(field_name), "max({})".format(field_name)).first()
-            table_data.append(generate_random_data(field_type, col_min, col_max))
-    return table_data
+            col_min, col_max = schema_min_max[field_name]
+            if isinstance(field_type, StringType):
+                table_data.append(''.join(random.choice(string.ascii_letters) for i in range(10)))
+            elif isinstance(field_type, IntegerType):
+                table_data.append(random.randint(col_min, col_max))
+            elif isinstance(field_type, LongType):
+                table_data.append(random.randint(col_min, col_max))
+            elif isinstance(field_type, DoubleType):
+                table_data.append(random.uniform(col_min, col_max))
+            elif isinstance(field_type, BooleanType):
+                table_data.append(random.choice([True, False]))
+            elif isinstance(field_type, DateType):
+                col_min_date = datetime.datetime.strptime(col_min, "%Y-%m-%d").date()
+                col_max_date = datetime.datetime.strptime(col_max, "%Y-%m-%d").date()
+                random_date = col_min_date + datetime.timedelta(days=random.randint(0, (col_max_date - col_min_date).days))
+                table_data.append(random_date)
+            elif isinstance(field_type, TimestampType):
+                col_min_time = datetime.datetime.strptime(col_min, "%Y-%m-%d %H:%M:%S")
+                col_max_time = datetime.datetime.strptime(col_max, "%Y-%m-%d %H:%
