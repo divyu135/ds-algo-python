fb8fe22e6754f17383a45469b30886ea08beb61b
Divyajeet Chauhan <71348527+divyu135@users.noreply.github.com>
2023-03-02 09:55:52 -0500

    Update random_data.py

--- random_data.py
+++ random_data.py
@@ -1,39 +1,47 @@
-from pyspark.sql.functions import rand, create_map, struct, size, col, lit, when
-from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType, ArrayType, MapType
+import random
+from pyspark.sql.functions import explode, col, rand
 
-# Define a sample PySpark DataFrame with different data types
-df = spark.createDataFrame([(1, 2.0, "foo", [1, 2, 3], {'key1': 'value1', 'key2': 'value2'}, (4, "bar")),
-                            (2, 3.0, "bar", [4, 5], {'key3': 'value3', 'key4': 'value4'}, (5, "baz"))],
-                           ["col_int", "col_float", "col_string", "col_array", "col_map", "col_struct"])
+# Define a PySpark DataFrame
+df = ...
 
-# Define a function to generate random data based on the min-max range of a column
-def generate_random_data(min_val, max_val, data_type):
-    if isinstance(data_type, IntegerType):
-        return int(rand() * (max_val - min_val) + min_val)
-    elif isinstance(data_type, DoubleType):
-        return rand() * (max_val - min_val) + min_val
-    elif isinstance(data_type, StringType):
-        return str(rand() * (max_val - min_val) + min_val)
-    elif isinstance(data_type, ArrayType):
-        return [generate_random_data(min_val, max_val, data_type.elementType) for _ in range(3)]
-    elif isinstance(data_type, MapType):
-        return create_map([lit(str(i)).alias("key"), generate_random_data(min_val, max_val, data_type.valueType).alias("value")] for i in range(2))
-    elif isinstance(data_type, StructType):
-        return struct([generate_random_data(min_val, max_val, field.dataType).alias(field.name) for field in data_type.fields])
+# Define a function to generate random values based on min and max values of a column
+def generate_random_value(min_val, max_val, data_type):
+    if data_type == "integer":
+        return random.randint(min_val, max_val)
+    elif data_type == "double":
+        return round(random.uniform(min_val, max_val), 2)
+    elif data_type == "boolean":
+        return random.choice([True, False])
+    elif data_type == "string":
+        return ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(min_val, max_val)))
+    elif data_type == "date":
+        return (datetime.date.today() - datetime.timedelta(days=random.randint(min_val, max_val))).strftime('%Y-%m-%d')
     else:
         return None
 
-# Generate random data for each column based on the min-max range of the DataFrame
-df_min = df.agg(*(min(col(column)).alias(column) for column in df.columns))
-df_max = df.agg(*(max(col(column)).alias(column) for column in df.columns))
+# Define a function to unnest complex types and generate random values
+def generate_random_row(row):
+    for col_name, data_type in row.dtypes:
+        if "struct" in data_type:
+            for field_name, field_type in row.select(col(col_name + ".*")).schema.fields:
+                min_val, max_val = row.select(col_name + "." + field_name).agg({"*": "min"}, {"*": "max"}).first()
+                row[col_name + "_" + field_name] = generate_random_value(min_val, max_val, field_type.typeName())
+            row = row.drop(col_name)
+        elif "array" in data_type:
+            row = row.withColumn(col_name, explode(col(col_name)))
+        elif "map" in data_type:
+            pass # handle map type here
+        else:
+            min_val, max_val = row.agg({col_name: "min"}, {col_name: "max"}).first()
+            row[col_name] = generate_random_value(min_val, max_val, data_type)
+    return row
 
-min_values = df_min.first()
-max_values = df_max.first()
-
-random_data = [generate_random_data(min_val, max_val, data_type) for (min_val, max_val, data_type) in zip(min_values, max_values, df.schema.fields)]
-
-# Create a new PySpark DataFrame with the generated random data
-random_df = spark.createDataFrame([tuple(random_data)], schema=df.schema)
-
-# Show the generated random data DataFrame
-random_df.show()
+# Generate 10 random rows of data
+result = df.sample(False, 0.1, seed=42).limit(10).\
+    select("*").\
+    selectExpr("*", "rand() as random").\
+    orderBy("random").\
+    drop("random").\
+    rdd.map(generate_random_row).toDF()
+    
+result.show()
